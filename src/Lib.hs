93435954e8f8e9c94fdf3f2c692b02ff8682f4ad
Léo Gaspard <leo@gaspard.io>
2018-04-05 13:32:49 +0200

    Setup project skeleton

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (doTheThing) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Nix.Expr
+import Nix.Parser
+
+doTheThing :: IO ()
+doTheThing = do
+    expr <- parse "test-input.nix"
+    putStrLn $ indent expr
+
+parse :: FilePath -> IO NExpr
+parse f = do
+    expr <- parseNixFile f
+    case expr of
+        Success a -> return a
+        Failure e -> error $ show e
+
+indent :: NExpr -> String
+indent a = show a
