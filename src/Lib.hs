82efb2c351da05e4dcbfa42c5774629c6537f2f7
Léo Gaspard <leo@gaspard.io>
2018-04-05 14:31:23 +0200

    Add basic string support

--- src/Lib.hs
+++ src/Lib.hs
@@ -28,6 +28,7 @@ indent a = trace (show a) $ exprIndent a
 exprIndent :: NExpr -> String
 exprIndent expr = case expr of
     Fix (NConstant c) -> atomIndent c
+    Fix (NStr s) -> stringIndent s
     Fix (NList vals) -> "[" ++ concatMap (\x -> exprIndent x ++ " ") vals ++ "]"
     Fix (NSet binds) -> "{" ++ concatMap bindingIndent binds ++ "}"
     _ -> "non implemented"
@@ -52,3 +53,14 @@ atomIndent a = case a of
     NBool False -> "false"
     NNull -> "null"
     NUri t -> T.unpack t
+
+stringIndent :: NString NExpr -> String
+stringIndent s = case s of
+    DoubleQuoted t -> stringIndent (Indented t) -- ignore string type
+    Indented t -> "\"" ++ concatMap antiquotedIndent t ++ "\""
+
+-- TODO: escape the text
+antiquotedIndent :: Antiquoted T.Text NExpr -> String
+antiquotedIndent a = case a of
+    Plain t -> T.unpack t
+    Antiquoted e -> "non implemented"
