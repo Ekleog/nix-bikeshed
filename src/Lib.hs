e03227e3503229cd668ed21773458b69e30d0d90
Léo Gaspard <leo@gaspard.io>
2018-04-05 23:25:22 +0200

    Rename *Indent to *I

--- src/Lib.hs
+++ src/Lib.hs
@@ -29,29 +29,29 @@ parseStr s = case parseNixString s of
     Failure e -> error $ show e
 
 indent :: NExpr -> String
-indent a = trace (show a) $ exprIndent a
+indent a = trace (show a) $ exprI a
 
 -- TODO: handle priority of operators
-exprIndent :: NExpr -> String
-exprIndent expr = case expr of
-    Fix (NConstant c) -> atomIndent c
-    Fix (NStr s) -> stringIndent s
+exprI :: NExpr -> String
+exprI expr = case expr of
+    Fix (NConstant c) -> atomI c
+    Fix (NStr s) -> stringI s
     Fix (NSym s) -> T.unpack s
-    Fix (NList vals) -> "[" ++ intercalate " " (map exprIndent vals) ++ "]"
-    Fix (NSet binds) -> setIndent False binds
-    Fix (NRecSet binds) -> setIndent True binds
+    Fix (NList vals) -> "[" ++ intercalate " " (map exprI vals) ++ "]"
+    Fix (NSet binds) -> setI False binds
+    Fix (NRecSet binds) -> setI True binds
     Fix (NLiteralPath p) -> p
     Fix (NEnvPath p) -> "<" ++ p ++ ">"
-    Fix (NUnary op ex) -> unaryOpIndent op ex
-    Fix (NBinary op l r) -> binaryOpIndent op l r
-    Fix (NSelect set attr def) -> selectIndent set attr def
-    Fix (NHasAttr set attr) -> hasAttrIndent set attr
-    Fix (NAbs param expr) -> absIndent param expr
-    Fix (NApp f x) -> appIndent f x
-    Fix (NLet binds ex) -> letIndent binds ex
-    Fix (NIf cond then_ else_) -> ifIndent cond then_ else_
-    Fix (NWith set expr) -> stmtIndent "with" set expr
-    Fix (NAssert test expr) -> stmtIndent "assert" test expr
+    Fix (NUnary op ex) -> unaryOpI op ex
+    Fix (NBinary op l r) -> binaryOpI op l r
+    Fix (NSelect set attr def) -> selectI set attr def
+    Fix (NHasAttr set attr) -> hasAttrI set attr
+    Fix (NAbs param expr) -> absI param expr
+    Fix (NApp f x) -> appI f x
+    Fix (NLet binds ex) -> letI binds ex
+    Fix (NIf cond then_ else_) -> ifI cond then_ else_
+    Fix (NWith set expr) -> stmtI "with" set expr
+    Fix (NAssert test expr) -> stmtI "assert" test expr
 
 -- Less binds stronger
 exprPrio :: NExpr -> Int
@@ -115,50 +115,50 @@ paren s = "(" ++ s ++ ")"
 parenIf :: Bool -> String -> String
 parenIf cond = if cond then paren else id
 
-bindingIndent :: Binding NExpr -> String
-bindingIndent b = case b of
-    NamedVar path val -> pathIndent path ++ " = " ++ exprIndent val ++ ";"
+bindingI :: Binding NExpr -> String
+bindingI b = case b of
+    NamedVar path val -> pathI path ++ " = " ++ exprI val ++ ";"
     Inherit Nothing vars ->
-        "inherit " ++ intercalate " " (map keyNameIndent vars) ++ ";"
+        "inherit " ++ intercalate " " (map keyNameI vars) ++ ";"
     Inherit (Just s) vars ->
-        "inherit (" ++ exprIndent s ++ ") " ++
-            intercalate " " (map keyNameIndent vars) ++ ";"
+        "inherit (" ++ exprI s ++ ") " ++
+            intercalate " " (map keyNameI vars) ++ ";"
 
-pathIndent :: NAttrPath NExpr -> String
-pathIndent p = intercalate "." $ map keyNameIndent p
+pathI :: NAttrPath NExpr -> String
+pathI p = intercalate "." $ map keyNameI p
 
-keyNameIndent :: NKeyName NExpr -> String
-keyNameIndent kn = case kn of
+keyNameI :: NKeyName NExpr -> String
+keyNameI kn = case kn of
     StaticKey k -> T.unpack k
-    DynamicKey (Plain s) -> stringIndent s
-    DynamicKey (Antiquoted e) -> "${" ++ exprIndent e ++ "}"
+    DynamicKey (Plain s) -> stringI s
+    DynamicKey (Antiquoted e) -> "${" ++ exprI e ++ "}"
 
-atomIndent :: NAtom -> String
-atomIndent a = case a of
+atomI :: NAtom -> String
+atomI a = case a of
     NInt i -> show i
     NBool True -> "true"
     NBool False -> "false"
     NNull -> "null"
     NUri t -> T.unpack t
 
-stringIndent :: NString NExpr -> String
-stringIndent s = case s of
-    DoubleQuoted t -> stringIndent (Indented t) -- ignore string type
+stringI :: NString NExpr -> String
+stringI s = case s of
+    DoubleQuoted t -> stringI (Indented t) -- ignore string type
     Indented t -> "\"" ++ concatMap escapeAntiquoted t ++ "\""
 
 escapeAntiquoted :: Antiquoted T.Text NExpr -> String
 escapeAntiquoted a = case a of
     Plain t -> tail $ init $ show $ T.unpack t
-    Antiquoted e -> "${" ++ exprIndent e ++ "}"
+    Antiquoted e -> "${" ++ exprI e ++ "}"
 
 unOpStr :: NUnaryOp -> String
 unOpStr op = case op of
     NNeg -> "-"
     NNot -> "!"
 
-unaryOpIndent :: NUnaryOp -> NExpr -> String
-unaryOpIndent op ex =
-    unOpStr op ++ parenIf (unaryPrio op < exprPrio ex) (exprIndent ex)
+unaryOpI :: NUnaryOp -> NExpr -> String
+unaryOpI op ex =
+    unOpStr op ++ parenIf (unaryPrio op < exprPrio ex) (exprI ex)
 
 binOpStr :: NBinaryOp -> String
 binOpStr op = case op of
@@ -178,67 +178,67 @@ binOpStr op = case op of
     NDiv -> "/"
     NConcat -> "++"
 
-binaryOpIndent :: NBinaryOp -> NExpr -> NExpr -> String
-binaryOpIndent op l r =
-    parenIf (binaryPrio op <= exprPrio l) (exprIndent l) ++ " " ++
+binaryOpI :: NBinaryOp -> NExpr -> NExpr -> String
+binaryOpI op l r =
+    parenIf (binaryPrio op <= exprPrio l) (exprI l) ++ " " ++
     binOpStr op ++ " " ++
-    parenIf (binaryPrio op <= exprPrio r) (exprIndent r)
+    parenIf (binaryPrio op <= exprPrio r) (exprI r)
 
-selectIndent :: NExpr -> NAttrPath NExpr -> Maybe NExpr -> String
-selectIndent set attr def =
-    parenIf (selectPrio <= exprPrio set) (exprIndent set) ++
-    "." ++ pathIndent attr ++
+selectI :: NExpr -> NAttrPath NExpr -> Maybe NExpr -> String
+selectI set attr def =
+    parenIf (selectPrio <= exprPrio set) (exprI set) ++
+    "." ++ pathI attr ++
     maybe ""
-          (\x -> " or " ++ parenIf (selectPrio <= exprPrio x) (exprIndent x))
+          (\x -> " or " ++ parenIf (selectPrio <= exprPrio x) (exprI x))
           def
 
-hasAttrIndent :: NExpr -> NAttrPath NExpr -> String
-hasAttrIndent set attr =
-    parenIf (hasAttrPrio <= exprPrio set) (exprIndent set) ++
-    " ? " ++ pathIndent attr
+hasAttrI :: NExpr -> NAttrPath NExpr -> String
+hasAttrI set attr =
+    parenIf (hasAttrPrio <= exprPrio set) (exprI set) ++
+    " ? " ++ pathI attr
 
-absIndent :: Params NExpr -> NExpr -> String
-absIndent par ex = paramIndent par ++ ": " ++ exprIndent ex
+absI :: Params NExpr -> NExpr -> String
+absI par ex = paramI par ++ ": " ++ exprI ex
 
-paramIndent :: Params NExpr -> String
-paramIndent par = case par of
+paramI :: Params NExpr -> String
+paramI par = case par of
     Param p -> T.unpack p
-    ParamSet set Nothing -> paramSetIndent set
-    ParamSet set (Just n) -> paramSetIndent set ++ " @ " ++ T.unpack n
+    ParamSet set Nothing -> paramSetI set
+    ParamSet set (Just n) -> paramSetI set ++ " @ " ++ T.unpack n
 
-paramSetIndent :: ParamSet NExpr -> String
-paramSetIndent set = case set of
-    FixedParamSet m -> "{ " ++ paramSetIndentImpl m ++ " }"
-    VariadicParamSet m -> "{ " ++ paramSetIndentImpl m ++ ", ... }"
+paramSetI :: ParamSet NExpr -> String
+paramSetI set = case set of
+    FixedParamSet m -> "{ " ++ paramSetIImpl m ++ " }"
+    VariadicParamSet m -> "{ " ++ paramSetIImpl m ++ ", ... }"
 
-paramSetIndentImpl :: M.Map T.Text (Maybe NExpr) -> String
-paramSetIndentImpl set =
+paramSetIImpl :: M.Map T.Text (Maybe NExpr) -> String
+paramSetIImpl set =
     intercalate ", " (
         map (\(k, x) -> case x of
             Nothing -> T.unpack k
-            Just e -> T.unpack k ++ " ? " ++ exprIndent e
+            Just e -> T.unpack k ++ " ? " ++ exprI e
         ) (M.toList set)
     )
 
-appIndent :: NExpr -> NExpr -> String
-appIndent f x =
-    parenIf (appPrio < exprPrio f) (exprIndent f) ++ " " ++ exprIndent x
+appI :: NExpr -> NExpr -> String
+appI f x =
+    parenIf (appPrio < exprPrio f) (exprI f) ++ " " ++ exprI x
 
-setIndent :: Bool -> [Binding NExpr] -> String
-setIndent rec binds =
+setI :: Bool -> [Binding NExpr] -> String
+setI rec binds =
     (if rec then "rec " else "") ++
     (if binds == [] then "{}"
-     else "{ " ++ intercalate " " (map bindingIndent binds) ++ " }")
+     else "{ " ++ intercalate " " (map bindingI binds) ++ " }")
 
-letIndent :: [Binding NExpr] -> NExpr -> String
-letIndent binds ex =
-    "let " ++ intercalate " " (map bindingIndent binds) ++ " in " ++
-    exprIndent ex
+letI :: [Binding NExpr] -> NExpr -> String
+letI binds ex =
+    "let " ++ intercalate " " (map bindingI binds) ++ " in " ++
+    exprI ex
 
-ifIndent :: NExpr -> NExpr -> NExpr -> String
-ifIndent cond then_ else_ =
-    "if " ++ exprIndent cond ++ " then " ++ exprIndent then_ ++
-    " else " ++ exprIndent else_;
+ifI :: NExpr -> NExpr -> NExpr -> String
+ifI cond then_ else_ =
+    "if " ++ exprI cond ++ " then " ++ exprI then_ ++
+    " else " ++ exprI else_;
 
-stmtIndent :: String -> NExpr -> NExpr -> String
-stmtIndent kw it expr = kw ++ " " ++ exprIndent it ++ "; " ++ exprIndent expr
+stmtI :: String -> NExpr -> NExpr -> String
+stmtI kw it expr = kw ++ " " ++ exprI it ++ "; " ++ exprI expr
--- test/Spec.hs
+++ test/Spec.hs
@@ -26,74 +26,74 @@ main = hspec $ do
                         (Fix (NConstant NNull))
                 ])
 
-    describe "exprIndent" $ do
+    describe "exprI" $ do
         it "indents constants" $ do
-            exprIndent (parseStr "42") `shouldBe` "42"
-            exprIndent (parseStr "true") `shouldBe` "true"
-            exprIndent (parseStr "false") `shouldBe` "false"
-            exprIndent (parseStr "null") `shouldBe` "null"
-            exprIndent (parseStr "https://example.org") `shouldBe`
+            exprI (parseStr "42") `shouldBe` "42"
+            exprI (parseStr "true") `shouldBe` "true"
+            exprI (parseStr "false") `shouldBe` "false"
+            exprI (parseStr "null") `shouldBe` "null"
+            exprI (parseStr "https://example.org") `shouldBe`
                 "https://example.org"
         it "indents strings" $ do
-            exprIndent (parseStr "\"foo bar\"") `shouldBe` "\"foo bar\""
-            exprIndent (parseStr "''foo bar''") `shouldBe` "\"foo bar\""
-            exprIndent (parseStr "''foo\"bar''") `shouldBe` "\"foo\\\"bar\""
-            exprIndent (parseStr "''foo\"bar''") `shouldBe` "\"foo\\\"bar\""
+            exprI (parseStr "\"foo bar\"") `shouldBe` "\"foo bar\""
+            exprI (parseStr "''foo bar''") `shouldBe` "\"foo bar\""
+            exprI (parseStr "''foo\"bar''") `shouldBe` "\"foo\\\"bar\""
+            exprI (parseStr "''foo\"bar''") `shouldBe` "\"foo\\\"bar\""
         it "indents lists" $ do
-            exprIndent (parseStr "[1 2 3]") `shouldBe` "[1 2 3]"
+            exprI (parseStr "[1 2 3]") `shouldBe` "[1 2 3]"
         it "indents sets" $ do
-            exprIndent (parseStr "{a=3;c=5;}") `shouldBe` "{ a = 3; c = 5; }"
-            exprIndent (parseStr "{inherit foo;}") `shouldBe` "{ inherit foo; }"
-            exprIndent (parseStr "{inherit (x) a b c;}") `shouldBe`
+            exprI (parseStr "{a=3;c=5;}") `shouldBe` "{ a = 3; c = 5; }"
+            exprI (parseStr "{inherit foo;}") `shouldBe` "{ inherit foo; }"
+            exprI (parseStr "{inherit (x) a b c;}") `shouldBe`
                 "{ inherit (x) a b c; }"
-            exprIndent (parseStr "{${a}=3;}") `shouldBe` "{ ${a} = 3; }"
-            exprIndent (parseStr "{\"${a}\"=3;}") `shouldBe` "{ \"${a}\" = 3; }"
-            exprIndent (parseStr "{\"a${a}b\"=3;}") `shouldBe`
+            exprI (parseStr "{${a}=3;}") `shouldBe` "{ ${a} = 3; }"
+            exprI (parseStr "{\"${a}\"=3;}") `shouldBe` "{ \"${a}\" = 3; }"
+            exprI (parseStr "{\"a${a}b\"=3;}") `shouldBe`
                 "{ \"a${a}b\" = 3; }"
         it "indents symbols" $ do
-            exprIndent (parseStr "a-b") `shouldBe` "a-b"
-            exprIndent (parseStr "{a=b;b=a;}") `shouldBe` "{ a = b; b = a; }"
+            exprI (parseStr "a-b") `shouldBe` "a-b"
+            exprI (parseStr "{a=b;b=a;}") `shouldBe` "{ a = b; b = a; }"
         it "indents recursive sets" $ do
-            exprIndent (parseStr "rec{a=b;b=a;}") `shouldBe`
+            exprI (parseStr "rec{a=b;b=a;}") `shouldBe`
                 "rec { a = b; b = a; }"
         it "indents literal paths" $ do
-            exprIndent (parseStr "./foo.bar") `shouldBe` "./foo.bar"
+            exprI (parseStr "./foo.bar") `shouldBe` "./foo.bar"
         it "indents environment paths" $ do
-            exprIndent (parseStr "<nixpkgs/nixos>") `shouldBe` "<nixpkgs/nixos>"
+            exprI (parseStr "<nixpkgs/nixos>") `shouldBe` "<nixpkgs/nixos>"
         it "indents operator-based expressions" $ do
-            exprIndent (parseStr "-(1+2+3-5*6 == 7 && 8 > 9)") `shouldBe`
+            exprI (parseStr "-(1+2+3-5*6 == 7 && 8 > 9)") `shouldBe`
                 "-(((1 + 2) + 3) - 5 * 6 == 7 && 8 > 9)"
-            exprIndent (parseStr "-3") `shouldBe` "-3"
+            exprI (parseStr "-3") `shouldBe` "-3"
         it "indents select expressions" $ do
-            exprIndent (parseStr "({}.a or {b=1;}).b") `shouldBe`
+            exprI (parseStr "({}.a or {b=1;}).b") `shouldBe`
                 "({}.a or { b = 1; }).b"
-            exprIndent (parseStr "{a=2;}.a or (3+3)") `shouldBe`
+            exprI (parseStr "{a=2;}.a or (3+3)") `shouldBe`
                 "{ a = 2; }.a or (3 + 3)"
         it "indents ?-expressions" $ do
-            exprIndent (parseStr "{a=1;}?a && {} ? b") `shouldBe`
+            exprI (parseStr "{a=1;}?a && {} ? b") `shouldBe`
                 "{ a = 1; } ? a && {} ? b"
         it "indents lambdas" $ do
-            exprIndent (parseStr "foo: bar: foo+bar") `shouldBe`
+            exprI (parseStr "foo: bar: foo+bar") `shouldBe`
                 "foo: bar: foo + bar"
         it "indents function applications" $ do
-            exprIndent (parseStr "(foo: foo) 3") `shouldBe`
+            exprI (parseStr "(foo: foo) 3") `shouldBe`
                 "(foo: foo) 3"
-            exprIndent (parseStr "let foo = x: (x +1); in foo  3") `shouldBe`
+            exprI (parseStr "let foo = x: (x +1); in foo  3") `shouldBe`
                 "let foo = x: x + 1; in foo 3"
         it "indents let expressions" $ do
-            exprIndent (parseStr "let a=1; in a") `shouldBe`
+            exprI (parseStr "let a=1; in a") `shouldBe`
                 "let a = 1; in a"
         it "indents if-expressions" $ do
-            exprIndent (parseStr "if  a==b then 0 else 1") `shouldBe`
+            exprI (parseStr "if  a==b then 0 else 1") `shouldBe`
                 "if a == b then 0 else 1"
         it "indents with-blocks" $ do
-            exprIndent (parseStr "with {}; 12") `shouldBe`
+            exprI (parseStr "with {}; 12") `shouldBe`
                 "with {}; 12"
         it "indents assertions" $ do
-            exprIndent (parseStr "assert 1==2; null") `shouldBe`
+            exprI (parseStr "assert 1==2; null") `shouldBe`
                 "assert 1 == 2; null"
         it "indents parameter sets" $ do
-            exprIndent (parseStr "{foo, bar?0, baz?{x=1;}}: baz") `shouldBe`
+            exprI (parseStr "{foo, bar?0, baz?{x=1;}}: baz") `shouldBe`
                 "{ bar ? 0, baz ? { x = 1; }, foo }: baz"
-            exprIndent (parseStr "{foo, bar?0, baz?{x=1;},...}: baz") `shouldBe`
+            exprI (parseStr "{foo, bar?0, baz?{x=1;},...}: baz") `shouldBe`
                 "{ bar ? 0, baz ? { x = 1; }, foo, ... }: baz"
